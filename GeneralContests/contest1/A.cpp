81e427bf5dc6ab734a908486b600f33b58afc879
Youssef-M-Salama <yosefsalamanote6@gmail.com>
2025-08-11 10:33:28 +0300

    Add files via upload

---
+++ GeneralContests/contest1/A.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#define ll long long
+using namespace std;
+int main()
+{
+    int a, b, sum;
+    cin >> a >> b;
+    cout << (a ^ b);
+    //^ = XOR
+    return 0;
+}
---
+++ GeneralContests/contest1/B.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#define ll long long
+using namespace std;
+int main()
+{
+    ll n, m;
+    cin >> n;
+    if (n >= 7)
+    {
+        cout << "YES";
+    }
+    else
+    {
+        cout << "NO";
+    }
+
+    return 0;
+}
