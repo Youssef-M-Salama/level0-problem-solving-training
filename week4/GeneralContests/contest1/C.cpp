c793a4e1098068dddcef2d6540d366547f029c94
Youssef-M-Salama <yosefsalamanote6@gmail.com>
2025-08-11 10:31:13 +0300

    Add files via upload

---
+++ week4/GeneralContests/contest1/C.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+#define ll long long
+
+int main()
+{
+    int n;
+    cin >> n;
+    char c;
+    cin >> c;
+    cout << c;
+
+    return 0;
+}
---
+++ week4/GeneralContests/contest1/D.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    ll n;
+    cin >> n;
+    cout << (n / 100) % 10;
+    return 0;
+}
